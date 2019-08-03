8131ffa3644f51dae53a20bb14e7a4664e349b95
Marc Tremblay <marc.tremblay@protonmail.ch>
2019-08-03 07:28:21 +0200

    Skip publish config when Maven username and password are missing

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,57 +37,59 @@ dependencies {
     implementation(Dependencies.structurizrSpring)
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            artifact(sourcesJar.get())
-            artifact(javadocJar.get())
-            pom {
-                name.set("Structurizr Macros")
-                description.set("A collection of macros for Structurizr")
-                url.set("https://github.com/sapientpants/structurizr-macros")
-                licenses {
-                    license {
-                        name.set("The MIT License")
-                        url.set("http://www.opensource.org/licenses/MIT")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("sapientpants")
-                        name.set("Marc Tremblay")
-                        email.set("marc.tremblay@gmail.com")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git@github.com:sapientpants/architecture-documentation-macros.git")
-                    developerConnection.set("scm:git:git@github.com:sapientpants/architecture-documentation-macros.git")
+if (extra.has("maven.publish.username") && extra.has("maven.publish.password")) {
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components["java"])
+                artifact(sourcesJar.get())
+                artifact(javadocJar.get())
+                pom {
+                    name.set("Structurizr Macros")
+                    description.set("A collection of macros for Structurizr")
                     url.set("https://github.com/sapientpants/structurizr-macros")
+                    licenses {
+                        license {
+                            name.set("The MIT License")
+                            url.set("http://www.opensource.org/licenses/MIT")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("sapientpants")
+                            name.set("Marc Tremblay")
+                            email.set("marc.tremblay@gmail.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:git@github.com:sapientpants/architecture-documentation-macros.git")
+                        developerConnection.set("scm:git:git@github.com:sapientpants/architecture-documentation-macros.git")
+                        url.set("https://github.com/sapientpants/structurizr-macros")
+                    }
                 }
             }
-        }
 
-        repositories {
-            maven {
-                val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-                val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
-                url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-                credentials {
-                    username = extra["maven.publish.username"] as String
-                    password = extra["maven.publish.password"] as String
+            repositories {
+                maven {
+                    val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+                    val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
+                    url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+                    credentials {
+                        username = extra["maven.publish.username"] as String
+                        password = extra["maven.publish.password"] as String
+                    }
                 }
             }
         }
     }
-}
 
-signing {
-    useGpgCmd()
-    setRequired({
-        (project.extra["isReleaseVersion"] as Boolean) && gradle.taskGraph.hasTask("uploadArchives")
-    })
-    sign(configurations.archives.get())
+    signing {
+        useGpgCmd()
+        setRequired({
+            (project.extra["isReleaseVersion"] as Boolean) && gradle.taskGraph.hasTask("uploadArchives")
+        })
+        sign(configurations.archives.get())
+    }
 }
 
 
