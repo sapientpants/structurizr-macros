6b8056eedd080bab9803bec21ab5ff575229fa58
Marc Tremblay <marc.tremblay@protonmail.ch>
2019-08-05 20:46:04 +0200

    Rename local variable for SystemLandscapeView

--- src/main/kotlin/io/github/sapientpants/structurizr/macros/SystemLandscapeView.kt
+++ src/main/kotlin/io/github/sapientpants/structurizr/macros/SystemLandscapeView.kt
@@ -4,7 +4,7 @@ import com.structurizr.view.ViewSet
 
 object SystemLandscapeView {
     fun addToViews(key: String, description: String, views: ViewSet) {
-        val contextView = views.createSystemLandscapeView(key, description)
-        contextView.addAllElements()
+        val systemLandscapeView = views.createSystemLandscapeView(key, description)
+        systemLandscapeView.addAllElements()
     }
 }
