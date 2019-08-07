15e4aa2d77c81667c4ec5985c2138a0dd22a1635
Marc Tremblay <marc.tremblay@protonmail.ch>
2019-08-07 07:04:28 +0200

    Refactor to use #addNearestNeighbours(component)

--- src/main/kotlin/io/github/sapientpants/structurizr/macros/ComponentViews.kt
+++ src/main/kotlin/io/github/sapientpants/structurizr/macros/ComponentViews.kt
@@ -1,10 +1,6 @@
 package io.github.sapientpants.structurizr.macros
 
-import com.structurizr.model.Component
-import com.structurizr.model.Container
-import com.structurizr.model.Model
 import com.structurizr.model.SoftwareSystem
-import com.structurizr.view.ComponentView
 import com.structurizr.view.ViewSet
 
 object ComponentViews {
@@ -20,54 +16,7 @@ object ComponentViews {
                 container.components
                     .forEach { component ->
                         componentView.add(component)
-                        addRelatedContainers(
-                            softwareSystem,
-                            component,
-                            componentView
-                        )
-                        addRelatedSoftwareSystems(
-                            softwareSystem.model,
-                            component,
-                            componentView
-                        )
-
-                        component.relationships
-                            .forEach { relationship ->
-                                val other = if (relationship.destination == component) {
-                                    relationship.source
-                                } else {
-                                    relationship.destination
-                                }
-
-                                when (other) {
-                                    is Container -> componentView.add(other)
-                                    is SoftwareSystem -> componentView.add(other)
-                                }
-                            }
-                    }
-            }
-    }
-
-    private fun addRelatedContainers(softwareSystem: SoftwareSystem, component: Component, view: ComponentView) {
-        softwareSystem.containers
-            .forEach { container ->
-                container.relationships
-                    .forEach { relationship ->
-                        if (relationship.destination == component || relationship.source == component) {
-                            view.add(container)
-                        }
-                    }
-            }
-    }
-
-    private fun addRelatedSoftwareSystems(model: Model, component: Component, view: ComponentView) {
-        model.softwareSystems
-            .forEach { softwareSystem ->
-                softwareSystem.relationships
-                    .forEach { relationship ->
-                        if (relationship.destination == component || relationship.source == component) {
-                            view.add(softwareSystem)
-                        }
+                        componentView.addNearestNeighbours(component)
                     }
             }
     }
