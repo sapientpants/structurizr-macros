9e528264d44451bc83c472921f2b89d52fd8eb36
Marc Tremblay <marc.tremblay@protonmail.ch>
2019-08-07 21:59:19 +0200

    Add package documentation

---
+++ src/main/java/io/github/sapientpants/structurizr/macros/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Macros (or procedures) to make using Structurizr easier to use by reducing
+ * the amount of typing one must do and to do things in a consistent manner.
+ */
+package io.github.sapientpants.structurizr.macros;
