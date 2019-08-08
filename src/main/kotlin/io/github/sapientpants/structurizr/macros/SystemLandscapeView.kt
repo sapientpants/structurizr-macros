5be4feafc830591f043191beef8c9389d704ade9
Marc Tremblay <marc.tremblay@protonmail.ch>
2019-08-08 22:00:51 +0200

    Add documentation to SystemLandscapeView

--- src/main/kotlin/io/github/sapientpants/structurizr/macros/SystemLandscapeView.kt
+++ src/main/kotlin/io/github/sapientpants/structurizr/macros/SystemLandscapeView.kt
@@ -3,7 +3,16 @@ package io.github.sapientpants.structurizr.macros
 import com.structurizr.model.Enterprise
 import com.structurizr.view.ViewSet
 
+/**
+ * Macros to add system landscape views.
+ * @see com.structurizr.view.SystemLandscapeView
+ */
 object SystemLandscapeView {
+    /**
+     * Adds a system landscape view to the workspace.
+     * @params enterprise the enterprise to use
+     * @params views the set of views
+     */
     fun addToViews(enterprise: Enterprise, views: ViewSet) {
         val systemLandscapeView = views.createSystemLandscapeView(
             "SystemLandscape-${enterprise.name}",
