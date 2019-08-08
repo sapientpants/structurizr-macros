6f00f7e875c2c49e94cbe58a2a42db3a037fd723
Marc Tremblay <marc.tremblay@protonmail.ch>
2019-08-08 22:01:06 +0200

    Add documentation to SystemContextView

--- src/main/kotlin/io/github/sapientpants/structurizr/macros/SystemContextView.kt
+++ src/main/kotlin/io/github/sapientpants/structurizr/macros/SystemContextView.kt
@@ -3,7 +3,16 @@ package io.github.sapientpants.structurizr.macros
 import com.structurizr.model.SoftwareSystem
 import com.structurizr.view.ViewSet
 
+/**
+ * Macros to add system context views.
+ * @see com.structurizr.view.SystemContextView
+ */
 object SystemContextView {
+    /**
+     * Adds a system context view to the workspace.
+     * @params softwareSystem the software system of interest
+     * @params views the set of views
+     */
     fun addToViews(softwareSystem: SoftwareSystem, views: ViewSet) {
         val systemContextView = views.createSystemContextView(
             softwareSystem,
