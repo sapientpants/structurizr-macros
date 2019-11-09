c248f7c093c36f36de9a3bdbcf8677100ff92dce
Marc Tremblay <marc.tremblay@protonmail.ch>
2019-11-09 22:34:48 +0100

    Rename SpringBootComponents to SpringComponents. Add exclusions and urlClassLoader support.

--- src/main/kotlin/io/github/sapientpants/structurizr/macros/SpringComponents.kt
+++ src/main/kotlin/io/github/sapientpants/structurizr/macros/SpringComponents.kt
@@ -7,6 +7,7 @@ import com.structurizr.analysis.SpringComponentFinderStrategy
 import com.structurizr.model.Component
 import com.structurizr.model.Container
 import java.io.File
+import java.net.URLClassLoader
 
 /**
  * Macros to add Components to a Container using Spring annotations.
@@ -19,48 +20,28 @@ import java.io.File
  * @see org.springframework.stereotype.Repository
  * @see org.springframework.stereotype.Service
  */
-object SpringBootComponents {
-    /**
-     * Tag for components created from Spring's @Component annotation.
-     *
-     * @see org.springframework.stereotype.Component
-     */
-    const val SPRING_COMPONENT_TAG = "Spring Component"
-
-    /**
-     * Tag for components created from Spring's @Controller annotation.
-     *
-     * @see org.springframework.stereotype.Controller
-     */
-    const val SPRING_MVC_CONTROLLER_TAG = "Spring MVC Controller"
-
-    /**
-     * Tag for components created from Spring's @Service annotation.
-     *
-     * @see org.springframework.stereotype.Service
-     */
-    const val SPRING_SERVICE_TAG = "Spring Service"
-
-    /**
-     * Tag for components created from Spring's @Repository annotation.
-     *
-     * @see org.springframework.stereotype.Repository
-     */
-    const val SPRING_REPOSITORY_TAG = "Spring Repository"
-
+object SpringComponents {
     /**
      * Finds Spring components and adds them to the specified container.
      *
      * @params container the container to which discovered components should be added
      * @params packageName the base package name to used when scanning for components
      * @params srcPath the path to the source code to scan
+     * @params exclusions regexes of types to be excluded during the component finding process
+     * @params urlClassLoader classloader to load classes from instead of the system classloader
      * @see com.structurizr.analysis.SpringComponentFinderStrategy
      * @see org.springframework.stereotype.Component
      * @see org.springframework.stereotype.Controller
      * @see org.springframework.stereotype.Repository
      * @see org.springframework.stereotype.Service
      */
-    fun addToContainer(container: Container, packageName: String, srcPath: String) {
+    fun addToContainer(
+        container: Container,
+        packageName: String,
+        srcPath: String,
+        vararg exclusions: String = emptyArray(),
+        urlClassLoader: URLClassLoader? = null
+    ) {
         val springComponentFinderStrategy =
             SpringComponentFinderStrategy(ReferencedTypesSupportingTypesStrategy(false))
         springComponentFinderStrategy.setIncludePublicTypesOnly(false)
@@ -71,6 +52,13 @@ object SpringBootComponents {
             springComponentFinderStrategy,
             SourceCodeComponentFinderStrategy(File(srcPath), 150)
         )
+
+        if (!exclusions.isNullOrEmpty()) {
+            componentFinder.exclude(*exclusions)
+        }
+
+        componentFinder.urlClassLoader = urlClassLoader
+
         val components = componentFinder.findComponents()
 
         components.forEach { c -> addTagsToComponent(c) }
@@ -86,10 +74,10 @@ object SpringBootComponents {
      */
     private fun addTagsToComponent(component: Component) {
         when (component.technology) {
-            SpringComponentFinderStrategy.SPRING_COMPONENT -> component.addTags(SPRING_COMPONENT_TAG)
-            SpringComponentFinderStrategy.SPRING_MVC_CONTROLLER -> component.addTags(SPRING_MVC_CONTROLLER_TAG)
-            SpringComponentFinderStrategy.SPRING_SERVICE -> component.addTags(SPRING_SERVICE_TAG)
-            SpringComponentFinderStrategy.SPRING_REPOSITORY -> component.addTags(SPRING_REPOSITORY_TAG)
+            SpringComponentFinderStrategy.SPRING_COMPONENT -> component.addTags(Tags.SPRING_COMPONENT)
+            SpringComponentFinderStrategy.SPRING_MVC_CONTROLLER -> component.addTags(Tags.SPRING_MVC_CONTROLLER)
+            SpringComponentFinderStrategy.SPRING_SERVICE -> component.addTags(Tags.SPRING_SERVICE)
+            SpringComponentFinderStrategy.SPRING_REPOSITORY -> component.addTags(Tags.SPRING_REPOSITORY)
         }
     }
 }
