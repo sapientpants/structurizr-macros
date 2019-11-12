7e7bf8580b660f6c17569a510ceec59183d0cc86
Marc Tremblay <marc.tremblay@protonmail.ch>
2019-11-12 20:53:32 +0100

    Revert "Make output dir when rendering with PlantUML"
    This reverts commit 819c211ab6fb3c5ae07638597459b5a1a1b80a69.

--- src/main/kotlin/io/github/sapientpants/structurizr/macros/renderer/PlantUmlRenderer.kt
+++ src/main/kotlin/io/github/sapientpants/structurizr/macros/renderer/PlantUmlRenderer.kt
@@ -63,9 +63,7 @@ object PlantUmlRenderer {
 
         val sourceFileReader = SourceFileReader(plantUmlFile)
         val image = sourceFileReader.generatedImages.first()
-        val outputDir = File(outputDirectoryPath)
-        outputDir.mkdirs()
-        val outputFile = File(outputDir, filename)
+        val outputFile = File("$outputDirectoryPath/$filename")
         FileUtils.copyToFile(image.pngFile, outputFile)
     }
 }
